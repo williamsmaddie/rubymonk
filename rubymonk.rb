a3680c267c7caeaac9c4daa8fe1b3951e920b756
madeleine williams <madeleine@mymilkcrate.com>
2019-08-28 18:56:28 -0400

    more ruby

---
+++ rubymonk.rb
@@ -0,0 +1,16 @@
+# rubymonk.com
+# 0.0
+puts self
+puts 2.even?
+puts 1.next
+puts 1.next.next
+# puts 1.methods.sort
+
+# 0.1
+puts ['rock','paper','scissors'].index('paper')
+puts 2.between? 1,3
+
+# stopped at
+# https://rubymonk.com/learning/books/1-ruby-primer/chapters/6-objects/lessons/37-syntactic-sugar-for-special-methods
+puts 4.+(3)
+puts 4+3
